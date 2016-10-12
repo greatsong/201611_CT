9b585f8c0c0beda8da154e83f6502e73efc194dd
greatsong <greatsong21@gmail.com>
2016-10-13 08:03:18 +0900

    연수준비(161012) 코드 일부 수정

--- 도전문제해결03-삼각화단만들기.py
+++ 도전문제해결03-삼각화단만들기.py
@@ -1,9 +1,9 @@
 n = int(input())
-
 cnt = 0
-for a in range(1, n+1) :
-    for b in range(a, n+1) :
-        for c in range(b, n+1):
-            if a + b + c == n and a+b > c :
+
+for a in range(1, int(n/3)+1) :
+    for b in range(int((n-2*a)/2), int((n-a)/2)+1) :
+        c = n - a - b
+        if a + b + c == n and  c > 0 :
                 cnt += 1
 print(cnt)
