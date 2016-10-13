4556c7a6eb86c77995f12ced0c4d76a9af0597a5
greatsong <greatsong21@gmail.com>
2016-10-13 11:43:47 +0900

    연수준비(161013) 광역시 문제, 삼각화단 O(n) 알고리즘

--- 도전문제해결03-삼각화단만들기.py
+++ 도전문제해결03-삼각화단만들기.py
@@ -6,4 +6,15 @@ for a in range(1, int(n/3)+1) :
         c = n - a - b
         if a + b + c == n and  c > 0 :
                 cnt += 1
+'''
+for a in range(1, n//3 + 1) :
+    b = (n-a)//2 - (n-2*a)//2
+    c = (n-a)//2 - a + 1
+    if b<c :
+        cnt += b
+    else :
+        cnt += c
+'''
+
 print(cnt)
+
